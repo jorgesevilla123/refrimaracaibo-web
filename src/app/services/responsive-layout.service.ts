0adddb24d339e79b9742e9bda38df521d9500339
Jorge Sevilla <jsdelduca@hotmail.com>
2025-04-28 15:18:06 -0400

    getting shipping changes

--- src/app/services/responsive-layout.service.ts
+++ src/app/services/responsive-layout.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, inject } from '@angular/core';
+import { Injectable, computed, inject } from '@angular/core';
 import { BreakpointObserver } from '@angular/cdk/layout';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 
 @Injectable({
@@ -14,7 +15,15 @@ export class ResponsiveLayoutService {
 
   breakPointObserver = inject(BreakpointObserver)
 
-  screenWidth$ = this.breakPointObserver.observe([this.small, this.medium, this.large]);
+  screenWidth = toSignal(this.breakPointObserver.observe([this.small, this.medium, this.large]));
+
+  smallWidth = computed(() => this.screenWidth()?.breakpoints[this.small])
+  mediumWidth = computed(() => this.screenWidth()?.breakpoints[this.medium])
+  largeWidth = computed(() => this.screenWidth()?.breakpoints[this.large])
+
+
+
+
 
   constructor() { }
 }
